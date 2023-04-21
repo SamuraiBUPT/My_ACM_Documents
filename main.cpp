d0540fa7253fd66efc2a867fb1a8261e3fbc4567
SamuraiBUPT <2781374337@qq.com>
2023-04-22 00:03:16 +0800

    数水坑-连通块问题

--- main.cpp
+++ main.cpp
@@ -9,67 +9,47 @@
 #include <math.h>
 
 using namespace std;
-
-const unsigned int N = 5000001;
-
-int num[N];
-int num_sum = 0; // sum先置为0
-int bu;
-int cnt = 0;
-int n, k;
-
-bool judge(int num_)
+char a[101][101];
+int ans;
+int n, m;
+void dfs(int x, int y)
 {
-    if (num_ <= 3)
+    a[x][y] = '.';
+    int dx, dy;
+    for (int i = -1; i <= 1; i++)
     {
-        return num_ > 1;
-    }
-    if (num_ % 6 != 1 && num_ % 6 != 5)
-    {
-        // 优化：不是6x+1或者6x-1的数，肯定不是质数
-        return false;
-    }
-    int temp = int(sqrt(num_));
-    for (int i = 5; i <= temp; i += 6)
-    {
-        if (num_ % i == 0 || num_ % (i + 2) == 0)
+        for (int j = -1; j <= 1; j++)
         {
-            return false;
+            dx = x + i;
+            dy = y + j;
+            if (dx >= 0 && dx <= n && dy >= 0 && dy < m && a[dx][dy] == 'W')
+            {
+                dfs(dx, dy);
+            }
         }
     }
-    return true;
+    return;
 }
-
-void dfs(int m, int sum, int startx)
-{ // 最重要的递归
-    // m代表现在选择了多少个数
-    // sum表示当前的和
-    // startx表示升序排列，以免算重
-    if (m == k)
-    {                   // 如果选完了的话
-        if (judge(sum)) // 如果和是素数
-            cnt++;      // ans加一
-        return;
-    }
-    for (int i = startx; i < n; i++)
-        dfs(m + 1, sum + num[i], i + 1); // 递归
-    // 步数要加一，和也要加
-    // 升序起始值要变成i+1,以免算重
-    return; // 这一个步骤下，所有的都枚举完了
-    // 直接返回去
-}
-
 int main()
 {
-
     freopen("in.txt", "r", stdin);
-    cin >> n >> k;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> num[i];
-    }
+    cin >> n >> m;
 
-    dfs(0, 0, 0);
-    cout << cnt << endl;
+    for (int i = 0; i <= n; i++)
+    {
+        cin >> a[i]; // 避免换行带来问题这里直接读入字符串
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (a[i][j] == 'W')
+            { // 如果是W的话就直接开始遍历
+                dfs(i, j);
+                ans++; // 水潭加一处
+            }
+        }
+    }
+    cout << ans;
     return 0;
 }
